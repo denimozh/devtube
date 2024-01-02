c0a6d77aba1b2b4302317c8cbd53bd23c0817d9d
denimozh <denimozh@gmail.com>
2024-01-02 17:43:46 +0000

    final change completed project is done

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -3,11 +3,11 @@ import { CheckCircle } from '@mui/icons-material'
 import { Link } from 'react-router-dom'
 import { demoProfilePicture } from '../utils/constants'
 
-const ChannelCard = ({ channelDetail }) => {
+const ChannelCard = ({ channelDetail, padding }) => {
   return (
-    <div className=' bg-black width-[358px] border-0 rounded-[20px] flex content-center items-center justify-center'>
+    <div className='  width-[358px] border-0 rounded-[20px] flex content-center items-center justify-center'>
       <Link to={`/channel/${channelDetail?.id?.channelId}`}>
-        <div className=' text-center text-white align-middle flex flex-col items-center justify-center pt-12'>
+        <div className={`text-center text-white align-middle flex flex-col items-center justify-center ${padding}`}>
           <img src={channelDetail?.snippet?.thumbnails?.high?.url || demoProfilePicture} alt={channelDetail?.snippet?.title}
           className='rounded-full md:h-[250px] md:w-[250px] sm:w-[100px] sm:h-[100px]'/>
           <p className='font-medium text-xl pt-8'>{channelDetail?.snippet?.title}<CheckCircle sx={{ fontSize: 14, color: 'gray', ml: '5px'}}/></p>
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -5,7 +5,7 @@ import { Feed, Sidebar } from '../components'
 const Main = ({open}) => {
   const [selectedCategory, setSelectedCategory] = useState('New')
   return (
-    <div className='flex pt-3 max-w-full'>
+    <div className='flex pt-3 max-w-full px-5'>
       <Sidebar open={open} selectedCategory={selectedCategory} setSelectedCatergory={setSelectedCategory}/>
       <Feed selectedCategory={selectedCategory}/>
     </div>
